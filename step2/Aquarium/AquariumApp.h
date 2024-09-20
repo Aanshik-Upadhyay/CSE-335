d2a6c6d637220d2b22f4d489e3199e9614f083c7
Aanshik-Upadhyay <81273906+Aanshik-Upadhyay@users.noreply.github.com>
2024-09-20 14:37:34 -0400

    Add files via upload

---
+++ step2/Aquarium/AquariumApp.h
@@ -0,0 +1,25 @@
+/**
+* @file AquariumApp.h
+ * @author Aanshik Upadhyay
+ * @brief Main application class
+ */
+
+
+#ifndef AQUARIUMAPP_H
+#define AQUARIUMAPP_H
+
+#include <wx/wx.h>
+
+/**
+ * Main application class
+ */
+class AquariumApp : public wxApp
+{
+private:
+
+public:
+ bool OnInit() override;
+};
+
+
+#endif ///AQUARIUMAPP_H
